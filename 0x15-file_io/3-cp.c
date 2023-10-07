3cb928ed04c3a3ac965d824c422df9acf0fcb6b7
99omniaashraf <omniaashraf249@gmail.com>
2023-10-06 17:52:55 -0700

    a

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,110 +1,47 @@
 #include "main.h"
 
 /**
- * close_file - close file
- * @fd: input
- */
-void close_file(int fd)
-{
-	if (close(fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", fd);
-		exit(100);
-	}
-}
-
-/**
- * error_handler - error handler
- * @fd: input1
- * @fname: input2
- * @type: input3
- * @buffer: input4
- * Return: 0 on success, exit status on error
- */
-int error_handler(int fd, char *fname, int type, char *buffer)
-{
-	int status = 0;
-	(void)fd;
-
-	switch (type)
-	{
-		case READ_ERROR:
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fname);
-			if (buffer != NULL)
-				free(buffer);
-			status = 98;
-			break;
-		case WRITE_ERROR:
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fname);
-			if (buffer != NULL)
-				free(buffer);
-			status = 99;
-			break;
-		default:
-			break;
-	}
-	return (status);
-}
-
-/**
- * create_buffer - create buffer
- * @filename: input
- * Return: pointer to new allocated buffer
- */
-char *create_buffer(char *filename)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * BUFFERSIZE);
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-	return (buffer);
-}
-
-/**
  * main - check of the code
  * @argc: input1
  * @argv: input2
  * Return: 0 success
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int fd_src, fd_dest, res;
-	char *buffer;
+	int fd_in, fd_out, bytes_read, bytes_written;
+	char buf[BUF_SIZE];
 
-	(void)argc;
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	fd_src = open(argv[1], O_RDONLY);
-	if (fd_src < -1)
-		exit(error_handler(fd_src, argv[1], READ_ERROR, NULL));
+	fd_in = open(argv[1], O_RDONLY);
+	if (fd_in < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
-	buffer = create_buffer(argv[2]);
-	umask(0);
-	fd_dest = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fd_dest < -1)
-		exit(error_handler(fd_dest, argv[2], WRITE_ERROR, buffer));
+	fd_out = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0666);
+	if (fd_out < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		exit(99);
+	}
 
-	do {
-		res = read(fd_src, buffer, BUFFERSIZE);
-		if (res == -1)
-			exit(error_handler(res, argv[1], READ_ERROR, buffer));
-		res = write(fd_dest, buffer, res);
-		if (res == -1)
-			exit(error_handler(res, argv[2], WRITE_ERROR, buffer));
-		fd_dest = open(argv[2], O_RDWR | O_APPEND);
-		if (fd_dest == -1)
-			exit(error_handler(fd_dest, argv[2], WRITE_ERROR, buffer));
-	} while (res > 0);
-	close_file(fd_src);
-	close_file(fd_dest);
-	free(buffer);
+	bytes_read = read(fd_in, buf, BUF_SIZE);
+	while (bytes_read > 0)
+	{
+		bytes_written = write(fd_out, buf, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	close(fd_in);
+	close(fd_out);
 	return (0);
 }
