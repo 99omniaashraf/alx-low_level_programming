10ad435a117c77cdac408a3fb6374226d70ccd0f
99omniaashraf <omniaashraf249@gmail.com>
2023-11-30 10:55:30 -0800

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,29 +8,29 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
-	hash_node_t **array;
+	hash_table_t *hash_table = NULL;
 	unsigned long int i;
 
 
-	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
 
-	array = malloc(sizeof(hash_node_t *) * size);
-	if (array == NULL)
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
 	{
 		free(hash_table);
 		return (NULL);
 	}
 
+	
 	for (i = 0; i < size; i++)
 	{
-		array[i] = NULL;
+		hash_table-array[i] = NULL;
 	}
-	table->array = array;
-	table->size = size;
-	return (table);
+	hash_table->array = array;
+	hash_table->size = size;
+	return (hash_table);
 }
